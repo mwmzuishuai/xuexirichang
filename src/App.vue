1ebab29ad7e404bae304960b1e2cb0e4492144ce
安阳彭于晏 <2713459649@qq.com>
2023-08-26 10:57:13 +0800

    萨达

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>明白</h1>
+    <h2 v-for="i in list">{{ i }}</h2>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+let list = ref([1, 2, 3, 4, 5, 6]);
+let fn = <N>(a: N): N => {
+  return a;
+};
+console.log(fn("hh"));
+</script>
